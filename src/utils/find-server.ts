ecdc5e4fca033ff1f44f72cae8ac9c94f1f41bbd
Giuseppe Basile <giuseppe@novoda.com>
2024-07-14 12:52:28 +0200

    script to find a server

---
+++ src/utils/find-server.ts
@@ -0,0 +1,39 @@
+import { NS } from "@ns";
+
+/** @param {NS} ns */
+export async function main(ns: NS) {
+    let target = ns.args[0] as string
+
+    let path = traverse(ns, "home", target, "home")
+    if (path == "") {
+        ns.tprint("Could not find " + target)
+        return
+    }
+
+    ns.tprint(path)
+}
+
+function traverse(ns: NS, server: string, target: string, from: string): string {
+    if (server == target) {
+        return target
+    }
+    let nodes = ns.scan(server)
+    for (let i = 0; i < nodes.length; i++) {
+        let child = nodes[i]
+        if (child == from) {
+            continue
+        }
+        if (child == target) {
+            return server + " => " + target
+        }
+        let children = ns.scan(server)
+        if (children.length === 0) {
+            continue
+        }
+        let foundOn = traverse(ns, child, target, server)
+        if (foundOn != "") {
+            return server + " => " + foundOn
+        }
+    }
+    return ""
+}
