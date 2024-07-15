a8f246e2f5344e14d72e65e3e518832af8928a05
Giuseppe Basile <giuseppe@novoda.com>
2024-07-16 01:30:08 +0200

    virus dumb to rotate over juicy networks

--- src/utils/viruses/virus-dumb.ts
+++ src/utils/viruses/virus-dumb.ts
@@ -1,30 +1,35 @@
 import { NS } from "@ns";
+import { canGainControl } from "../server-hacking";
+import { notHackableServers } from "../server-exploring";
 
 /** @param {NS} ns */
 export async function main(ns: NS) {
-    const target = "n00dles";
+    const ideal_index = ns.args[0] as number;
     const availableThreads = ns.ps(ns.getHostname())
         .filter((process => process.filename == ns.getScriptName()))
         .reduce((acc, process) => process.threads + acc, 0);
 
-    // Defines how much money a server should have before we hack it
-    // In this case, it is set to the maximum amount of money.
-    const moneyThresh: number = ns.getServerMaxMoney(target) * 0.8;
-
-    // Defines the maximum security level the target server can have. 
-    // If the target's security level is higher than this, we'll weaken it before doing anything else
-    const securityThresh: number = ns.getServerMinSecurityLevel(target) * 1.3;
-
     // Infinite loop that continuously hacks/grows/weakens the target server
     while (true) {
+        const targets = ["n00dles", "zer0", "nectar-net", "omega-net"]
+            .filter((server) => canGainControl(ns, server))
+            .filter((server) => !notHackableServers.has(server))
+            .filter((server) => ns.getServerRequiredHackingLevel(server) < ns.getHackingLevel() / 2)
+        const target = targets[ideal_index % targets.length]
+
+        const moneyThresh: number = ns.getServerMaxMoney(target) * 0.8;
+        const securityThresh: number = ns.getServerMinSecurityLevel(target) * 1.3;
+
         if (ns.getServerSecurityLevel(target) > securityThresh) {
-            // If the server's security level is above our threshold, weaken it
             await ns.weaken(target);
         } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-            // If the server's money is less than our threshold, grow it
-            await ns.grow(target);
+            const currentMoney = ns.getServerMoneyAvailable(target);
+            const maxMoney = ns.getServerMaxMoney(target);
+            const multiplier = maxMoney / Math.max(currentMoney, 1);
+            const targetThreads = ns.growthAnalyze(target, multiplier);
+
+            await ns.grow(target, { threads: Math.min(targetThreads, availableThreads) });
         } else {
-            // Otherwise, hack it
             const targetThreads = ns.hackAnalyzeThreads(target, ns.getServerMaxMoney(target) * 0.1)
             const availableThreads = ns.getRunningScript("utils/viruses/virus-best-hack-10.js")?.threads ?? 1;
             if (await ns.hack(target, { threads: Math.min(targetThreads, availableThreads) }) == 0) {
