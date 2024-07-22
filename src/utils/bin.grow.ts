5a4064b4b4697d2b0a07158ece838cd2276b0936
Giuseppe Basile <giuseppe@novoda.com>
2024-07-23 00:52:29 +0200

    simplified base scripts

--- src/utils/bin.grow.ts
+++ src/utils/bin.grow.ts
@@ -3,6 +3,8 @@ import { NS } from "@ns";
 /** @param {NS} ns */
 export async function main(ns: NS) {
     let host = ns.args[0] as string
+    let delay = ns.args[1] as number
 
+    await ns.sleep(delay);
     await ns.grow(host);
 }
